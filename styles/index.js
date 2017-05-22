de71e104d3555f02e24dc991c50d2f723e8ae679
ncmikkelsen <nikolaj.c.mikkelsen@gmail.com>
2017-05-22 15:38:05 +0200

    Add div and button for aboutme section

--- styles/index.js
+++ styles/index.js
@@ -1,20 +1,41 @@
 var hide_lists = function(cb) {
-    $('#posts').fadeOut(300);
-    $('#projects').fadeOut(300);
+    $('#aboutme').fadeOut(200);
+    $('#posts').fadeOut(200);
+    $('#projects').fadeOut(200);
+
+    $('#aboutme-btn').removeClass('disabled');
     $('#posts-btn').removeClass('disabled');
     $('#projects-btn').removeClass('disabled')
 };
 var show_projects = function() {
+    $('#aboutme-btn').removeClass('disabled');
     $('#posts-btn').removeClass('disabled');
-    $('#posts').fadeOut(300, function() {
-        $('#projects').fadeIn(300)
+
+    $('#aboutme, #posts').fadeOut(200).promise().done(function(){
+        $('#projects').fadeIn(200)
     });
+
     $('#projects-btn').addClass('disabled')
 };
+
 var show_posts = function() {
+    $('#aboutme-btn').removeClass('disabled');
     $('#projects-btn').removeClass('disabled');
-    $('#projects').fadeOut(function() {
-        $('#posts').fadeIn(300)
+
+    $('#aboutme, #projects').fadeOut(200).promise().done(function(){
+        $('#posts').fadeIn(200)
     });
+
     $('#posts-btn').addClass('disabled')
+};
+
+var show_aboutme = function(){
+    $('#posts-btn').removeClass('disabled');
+    $('#projects-btn').removeClass('disabled');
+
+    $('#posts, #projects').fadeOut(200).promise().done(function(){
+        $('#aboutme').fadeIn(200)
+    });
+
+    $('#aboutme-btn').addClass('disabled')
 };
